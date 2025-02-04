193cf637c289cbdd3e5d64959dbaca1b1ef74e56
EliasHdzR <2230200@upv.edu.mx>
2025-02-04 01:16:05 -0600

    changed from blob to contours wip

---
+++ image_processor.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+from PyQt6 import QtGui
+from PyQt6.QtGui import QPixmap
+
+# aqui almacenamos los centros de TODAS las figuras y contornos y eso
+# [coordenada x, coordenada y, valores rgb de ese pixel]
+# green = respuestas seleccionadas, red = respuestas no seleccionadas
+centroids = []
+
+def ProcessImage(original_image):
+    # modificar imagen para reconocer solo respuestas seleccionadas
+    non_selected_answers_image = RecognizeNotSelectedAnswers(original_image)
+
+    RecognizeSelectedAnswers(original_image) # pa poner en verde las que sí son
+
+    # delimitar filas de respuestas
+    # DelimitateAnswersRows(original_image)
+
+    return non_selected_answers_image
+
+def RecognizeSelectedAnswers(original_image):
+    img_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(img_gray, (5, 5), 0)
+
+    # edge detection
+    edges = cv2.Canny(blurred_image, 210, 255)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    selected_answers_image = original_image.copy()
+
+    hierarchy = hierarchy[0]
+
+    for i,c in enumerate(contours):
+        area = cv2.contourArea(c)
+
+        # los contornos se generan en la imagen de arriba a abajo, de derecha a izquierda
+        # jerarquia de nodos [sig, anterior, hijo, padre] (checar consola)
+        # agarramos los que no tienen hijos ni padres
+        # esta jalada no agarra bien los que quiero ptm
+        # TODOS TIENEN QUE SER CIRCULOS CERRRADOS PARA QUE NO SE ARRUINE LA JERARQUIA (mover el threshold de area?)
+        if 80 < area < 500 and hierarchy[i][2] == -1 and hierarchy[i][3] == -1:
+            # pa calcular el centroide de cada contorno
+            moment = cv2.moments(c)
+            x = int(moment["m10"] / moment["m00"])
+            y = int(moment["m01"] / moment["m00"])
+            centroids.append((x, y, "green"))
+            #print("x:", x, " y:", y, " color:", "green")
+
+            selected_answers_image = cv2.drawContours(selected_answers_image, [c], 0, (0, 255, 0), 1)
+
+    print(hierarchy)
+    cv2.imshow("edges", edges)
+    cv2.imshow("selected answers", selected_answers_image)
+
+
+# fuck blob detection, i'm now edging
+def RecognizeNotSelectedAnswers(original_image):
+    img_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(img_gray, (5, 5), 0)
+
+    # edge detection
+    edges = cv2.Canny(blurred_image, 225, 255)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    non_selected_answers_image = original_image.copy()
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if 80 < area < 180:
+            # pa calcular el centroide de cada contorno
+            moment = cv2.moments(c)
+
+            x = int(moment["m10"] / moment["m00"])
+            y = int(moment["m01"] / moment["m00"])
+            centroids.append((x, y, "red"))
+
+            #print("x:", x, " y:", y, " color:", "red")
+
+            non_selected_answers_image = cv2.circle(non_selected_answers_image, (x, y), 5, (0, 0, 255), -1)
+
+    return non_selected_answers_image
+
+def DelimitateAnswersRows(original_image):
+    img_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    th, thresholded_image = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(image=thresholded_image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+    for c in contours:
+        # calculate centroids for each contour
+        area = cv2.contourArea(c)
+        if 10000 < area < 100000:
+            original_image = cv2.drawContours(original_image, [c], -1, (0, 255, 0), 2)
+
+    cv2.imshow("contourrsss", original_image)
+
+def CvToPixmap(cv_image):
+    # al parecer tienes que traducir(?) los pixeles de RGB a BGR (?????) pa mostrar la imagen en pyqt(?????????)
+    image_temp = QtGui.QImage(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB), cv_image.shape[1], cv_image.shape[0], cv_image.shape[1] * 3, QtGui.QImage.Format.Format_RGB888)
+    return QPixmap(image_temp)
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from PyQt6.QtCore import QSize, pyqtSignal, Qt
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QGridLayout, QWidget, QTextEdit, QFileDialog
-from PyQt6 import QtGui
 import cv2
-import numpy as np
+import image_processor
 
 class ImageFrame(QLabel):
     def __init__(self):
@@ -30,7 +28,7 @@ class Window(QWidget):
         self.buttonOpen = QPushButton("Open Image")
         BUTTON_SIZE = QSize(200, 50)
         self.buttonOpen.setMinimumSize(BUTTON_SIZE)
-        self.buttonOpen.clicked.connect(self.handleOpen)
+        self.buttonOpen.clicked.connect(self.HandleOpen)
 
         self.buttonProcess = QPushButton("Analyze Image")
         self.buttonProcess.setMinimumSize(BUTTON_SIZE)
@@ -53,70 +51,30 @@ class Window(QWidget):
         layout.setColumnStretch(3, 1)
         layout.setColumnStretch(4, 0)
 
-    def handleOpen(self):
-        start = "."
-
+    def HandleOpen(self):
+        start = "./fotos"
         path = QFileDialog.getOpenFileName(self, "Choose File", start, "Images(*.jpg *.png)")[0]
         if path == "": return
+        self.UpdateImage(path)
 
-        self.ActualizarImagen(path)
-
-    def ActualizarImagen(self, filepath):
+    def UpdateImage(self, filepath):
         self.originalImageFrame.clear()
         self.processedImageFrame.clear()
-        self.originalImage = cv2.imread(filepath)
+        self.answers.setText("Answers will be displayed here")
 
         # pa poder reescalar la imagen en el contenedor pero manteniendo un factor de escalado pa que no se vea feo
+        self.originalImage = cv2.imread(filepath)
         frame_width = self.originalImageFrame.size().width()
         scale_factor = frame_width / self.originalImage.shape[1]
         self.originalImage = cv2.resize(self.originalImage, None, fx= scale_factor, fy=scale_factor, interpolation=cv2.INTER_AREA)
 
-
-        pixmap = CvToPixmap(self.originalImage)
+        pixmap = image_processor.CvToPixmap(self.originalImage)
         self.originalImageFrame.setPixmap(pixmap)
 
     def ProcessImage(self):
-        #image = cv2.blur(self.originalImage, (5, 5)) # normal
-        #image = cv2.GaussianBlur(self.originalImage, (5, 5), 0) # normal
-        image = cv2.medianBlur(self.originalImage, 15)                               # este está bien culero jajaja
-        #image = cv2.bilateralFilter(self.originalImage, 5, 21, 8)       # de los mejores
-        th, im = cv2.threshold(image, 77, 255, cv2.THRESH_BINARY)
-
-        # Default values of parameters are tuned to extract dark circular blobs.
-        params = cv2.SimpleBlobDetector_Params()
-
-        params.filterByArea = True
-        params.maxArea = 100000
-
-        params.filterByCircularity = True
-        params.minCircularity = 0.5
-
-        params.filterByConvexity = True
-        params.minConvexity = 0.5
-
-        detector = cv2.SimpleBlobDetector_create(params)
-        keypoints = detector.detect(im)
-
-        im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
-                                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        imx = self.originalImage.copy()
-        for keypoint in keypoints:
-            print(keypoint)
-            x = int(keypoint.pt[0])
-            y = int(keypoint.pt[1])
-            s = keypoint.size / 2
-            if s > 7:
-                imx = cv2.circle(imx, (int(x), int(y)), int(s), (0, 255), 2)
-
-        cv2.imshow("Image", im_with_keypoints)
-        pixmap = CvToPixmap(imx)
+        self.processedImage = image_processor.ProcessImage(self.originalImage)
+        pixmap = image_processor.CvToPixmap(self.processedImage)
         self.processedImageFrame.setPixmap(pixmap)
-
-def CvToPixmap(cv_image):
-    # al parecer tienes que traducir(?) los pixeles de RGB a BGR (?????) pa mostrar la imagen en pyqt(?????????)
-    image_temp = QtGui.QImage(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB), cv_image.shape[1], cv_image.shape[0], cv_image.shape[1] * 3, QtGui.QImage.Format.Format_RGB888)
-    return QPixmap(image_temp)
 
 if __name__ == '__main__':
     import sys
